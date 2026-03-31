02b750bc7bd37680adbe36333d5d732df788d1b1
FrancoisL0001 <165661986+FrancisL0001@users.noreply.github.com>
2026-03-31 19:07:58 -0400

    Created basic backend files.

---
+++ backend/dataloader.py
@@ -0,0 +1,12 @@
+"""
+
+Data-Loader. Contains script to load data from various sources to and preprocessing them for use in my classifier
+
+"""
+
+import pandas as pd
+import numpy as np
+import sklearn 
+import matplotlib.pyplot as plt
+
+
